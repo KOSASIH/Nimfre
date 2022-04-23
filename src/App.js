962be638ea1e0d38d8d2d224cde21dc706fadced
KOSASIH <kosasihg88@gmail.com>
2022-04-24 05:13:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Nimfre">
+      <header className="Nimfre-header">
+        <img src={logo} className="Nimfre-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/Nimfre.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Nimfre-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
